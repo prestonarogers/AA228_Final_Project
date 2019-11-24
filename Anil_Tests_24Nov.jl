6f9d6fe835d5466cc024ab810bbbede1863a913f
Anil Yildiz <yldzanl@gmail.com>
2019-11-24 15:23:13 -0800

    Initial working model for the RC boat problem
    These are the things that need to be added/improved:
    
    1. I put one police boat for now, the second tuple in the state array (10,10) is the police, I assumed the police is stationary for now, we need to model the police's movement.
    2. There is no uncertainty in our observation, I put obs = sp for now, we need to model that as a multivariate distribution.
    3. We can add more boats (police, or sailboats) to the environment.
    4. Also, I assumed that we know our initial position perfectly (deterministic dist.), (5,5) as can be seen. We should make tha Gaussian too. (Notice that this is different that the multivariate dist. thing above).
    5. The legality of the actions is not taken into consideration. Therefore, depending on the action taken, next state can fall outside the bounds of allStates. I implemented a function actions(p::RCBoatProblem,s::AbstractArray) but this is disregarded by the Solver, and needs to be fixed.

---
+++ Anil_Tests_24Nov.jl
@@ -0,0 +1,122 @@
+using POMDPs
+using Distributions: Normal
+using Random
+import POMDPs: initialstate_distribution, actions, gen, discount, isterminal
+using POMDPModelTools # for Deterministic
+using BasicPOMCP
+using POMDPSimulators
+Random.seed!(1);
+
+
+
+mutable struct RCBoatProblem <: POMDPs.POMDP{AbstractArray,Tuple,AbstractArray}
+    # To simplify things, define every parameter related to the problem in this struct!
+    stateSpace::AbstractArray  # the entire GridWorld
+    actionSpace::AbstractArray  # all possible actions takeable (checks legallity later)
+    rockPositions::AbstractArray  # where are the rocks?
+    
+    homePosition::Tuple  # where am I trying to reach?
+    RC_initialPosition::Tuple  # where did the boat start at?
+    police_initialPosition::Tuple  # where did the police start at?
+
+    rockReward::Float64  # reward for hitting a rock (should be negative!)
+    policeReward::Float64  # reward for getting caught to a police boat (should be highly negative!)
+    movementReward::Float64  # for each action I take, there should be a cost (should be negative!)
+    reachingHomeReward::Float64  # reward for reaching home (should be highly positive!)
+    
+    gettingCaughtDist::Float64  # Eucledean distance that will make us get caught to the police
+    discountFactor::Float64  # discount factor (gamma)
+end
+
+
+# All possible states and actions
+allStates = [(i,j) for i in collect(1:20) for j in collect(1:20)];
+allActions = [(i,j) for i in collect(-1:1) for j in collect(-1:1)];
+
+# Define how many rocks there will be on the GridWorld, and place them randomly
+num_of_rocks_to_create = 30
+rocks = rand(allStates, num_of_rocks_to_create)
+
+pomdp = RCBoatProblem(allStates, allActions, rocks, (21,20), (5,5), (10,10), -5, -1000, -1, 10000, 10, 0.8)
+discount(p::RCBoatProblem) = p.discountFactor
+isterminal(p::RCBoatProblem, s::AbstractArray) = isequal(s[1],p.homePosition)
+initialstate_distribution(p::RCBoatProblem) = Deterministic([p.RC_initialPosition, p.police_initialPosition]);
+
+
+# Calculate the Euclidean distance between point a and b.
+function EuclideanDistance(a,b)
+    return sqrt((a[1]-b[1])^2 + (a[2]-b[2])^2)
+end
+
+# Check if the action is legal (does not go outside the limits of the lake).
+function isActionLegal(p::RCBoatProblem, s::AbstractArray, a::Tuple)
+    RC_state_next = Tuple(collect(s[1])+collect(a))  # just add s and a
+    if !(RC_state_next in p.stateSpace)  # if not in stateSpace
+        return false
+    else
+        return true
+    end
+end
+
+# All possible action, regardless of state (Throws error if you don't put this line).
+actions(p::RCBoatProblem) = allActions
+
+# Legal possible actions takeable in a certain state.
+function actions(p::RCBoatProblem,s::AbstractArray)
+    allowedActions = []
+    for a in p.actionSpace
+        if isActionLegal(p,s,a)
+            push!(allowedActions,a)
+        end
+    end
+    return allowedActions
+end
+
+noise(x) = ceil(Int, abs(x - 5)/sqrt(2) + 1e-2)
+
+function gen(p::RCBoatProblem, s::AbstractArray, a::Tuple, rng::AbstractRNG)
+    # generate next state
+    RC_state_now = s[1]
+    police_state_now = s[2]
+
+    RC_state_next = Tuple(collect(s[1])+collect(a))  # just add s and a
+    police_state_next = police_state_now
+
+    # sp (next state) is an Array <: AbstractArray of Tuples.
+    # First element of this Array should always be the RC boat.
+    sp = [RC_state_next, police_state_next]
+    
+    
+    # generate observation
+    o = sp
+
+    # generate reward
+
+    EuclDistReward = EuclideanDistance(p.homePosition,RC_state_now)
+
+    if RC_state_next in p.rockPositions  # hitting rock reward
+        r = p.rockReward - EuclDistReward
+    elseif isequal(RC_state_next, p.homePosition)  # reaching home reward
+        r = p.reachingHomeReward - EuclDistReward
+    else
+        r = p.movementReward - EuclDistReward  # just the reward for making a movement
+    end
+
+    return (sp=sp, o=o, r=r)
+
+end
+
+
+
+solver = POMCPSolver(tree_queries=1000, c=10)
+planner = solve(solver, pomdp);
+
+for (s,a,r,sp,o) in stepthrough(pomdp, planner, "s,a,r,sp,o")
+    @show (s,a,r,sp,o)
+end
+
+
+## This is for debugging. It will print the optimal action for a single state.
+# b = initialstate_distribution(pomdp)
+# a = action(planner, b)
+# println("Action $a selected for $b.")
